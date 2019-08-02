b164dba3a6ee2c16011591ae1dcce594a4c0277c
marcogomesr <marco.gomesr@nodelondon.com>
2019-08-02 16:25:47 +0100

    change baseUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
 		   * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
 		   * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
 		   */
-		  baseUrl: `localhost`,
+		  baseUrl: `wordpress-api.dev.node-production.com`,
 		  // The protocol. This can be http or https.
 		  protocol: `http`,
 		  // Indicates whether the site is hosted on wordpress.com.
